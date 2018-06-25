38d67a8fe6b6ac7523e8b8f40275c92fd317b157
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-06-25 23:20:17 +0530

    middle elememt using two pointers

--- middle.cpp
+++ middle.cpp
@@ -3,7 +3,7 @@ void List ::middle_Element(){
 	Node *low=head;
 	Node *fast=head;
 	if(head!=NULL){
-	while((fast!=NULL)&&(fast->next=NULL)){
+	while((fast!=NULL)&&(fast->next=!NULL)){
 		fast=fast->next->next;
 		low=low->next;
 	}
